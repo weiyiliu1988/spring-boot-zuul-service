57bbbf7f81be24ac8a5f09eb734699951ed403c8
edward <731839038@qq.com>
2018-04-21 23:42:19 +0800

    update

---
+++ src/main/java/cn/com/studyshop/ZuulServiceApplication.java
@@ -0,0 +1,29 @@
+package cn.com.studyshop;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import cn.com.studyshop.filter.AccessFilter;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ZuulServiceApplication.class).web(true).run(args);
+	}
+
+	/**
+	 *
+	 * IOC
+	 * 
+	 * @return
+	 */
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}
+
+}
